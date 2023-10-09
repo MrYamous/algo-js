ce8f4d9953b3ab1e5a573c00c8780c3d717dbbdd
Matthieu Lempereur <matt.lempereur@gmail.com>
2023-10-09 11:39:17 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+/*if(1 === '1') {
+    alert("1 egal 1")
+} else {
+    alert("Else")
+}*/
+
+const a = 4;
+const b = 4;
+const c = 3;
+
+if(a === b) {
+    console.log('egal')
+}
+
+if (c < b && a + c != 3) {
+    console.log('ok')
+} else {
+    console.log('rate')
+}
